26e850ab0ef7c68f8fefa4cb96801aaab83416cd
Harrisson06 <harrison.macdonald06@gmail.com>
2026-04-02 16:20:06 +0100

    added more functions

--- ConsoleApplication/ConsoleApplication/main.cpp
+++ ConsoleApplication/ConsoleApplication/main.cpp
@@ -5,6 +5,8 @@
 #include "ResourceList.h"
 #include "UserList.h"
 #include "Loan.h"
+#include <algorithm>
+
 using namespace std;
 
 int main() {
@@ -34,19 +36,84 @@ int main() {
         if (command == "exit") {
             break;
         }
+
         else if (command == "list") {
             resourceList.printResourceList();
         }
+
         else if (command == "report") {
             int type;
             iss >> type;
             if (type == 1) {
-                cout << "Resources currentyl loaned out:" << endl;
+                cout << "Resources currently loaned out:" << endl;
                 for (auto loan : loans) {
                     cout << loan->getPerson()->asString() << endl;
                 }
             }
         }
+
+        else if (command == "borrow") {
+            int userID;
+            string resourceID;
+            iss >> userID >> resourceID;
+
+            // Finding user
+            Person* user = nullptr;
+            for (auto u : userList.getList()) {
+                if (u->getID() == userID) {
+                    user = u;
+                    break;
+                }
+            }
+
+            // Finding resource
+            Resource* resource = nullptr;
+            for (auto r : resourceList.getList()) {
+                if (r->getID() == resourceID) {
+                    resource = r;
+                    break;
+                }
+            }
+
+            if (user == nullptr) {
+                cout << "User not found" << endl;
+            }
+
+            else if (resource == nullptr) {
+                cout << "Resource not found" << endl;
+            }
+
+            else if (!resource->getCanLend() || resource->getIsBorrowed()) {
+                cout << "Resource " << resourceID << " is currently unable to be borrowed" << endl;
+            }
+
+            else if (loans.size() >= user->getBorrowlimit()) {
+                cout << "User " << userID << " has reached their borrow limit" << endl;
+            }
+
+            else {
+                resource->setIsBorrowed(true);
+                loans.push_back(new Loan(user, resource));
+                cout << "Resource has been borrowed" << endl;
+            }
+        }
+
+        else if (command == "return") {
+            int userID;
+            string resourceID;
+            iss >> userID >> resourceID;
+
+            for (auto& loan : loans) {
+                if (loan->getPerson()->getID() == userID &&
+                    loan->getResource()->getID() == resourceID) {
+                    loan->getResource()->setIsBorrowed(false);
+                    loans.erase(remove(loans.begin(), loans.end(), loan), loans.end());
+                    cout << "Resource has been returned" << endl;
+                    break;
+                }
+            }
+        }
+
         else {
             cout << "unknown command." << endl;
         }
